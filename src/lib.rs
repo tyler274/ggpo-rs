097a68ddd1785f57670b22129f047aa83ba5d52c
Tyler274 <tyler274port@gmail.com>
2019-10-17 23:00:15 -0400

    init

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+extern crate arraydeque;
+extern crate bit_vec;
+extern crate log;
+extern crate mio;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
